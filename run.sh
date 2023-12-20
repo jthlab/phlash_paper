7417b4f88a3556fa9a5dcb8f0955f8e3d65619e5
Jonathan Terhorst <jonth@umich.edu>
2023-12-20 15:09:42 -0500

    add sbatch script

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+#SBATCH --time=24:00:00
+#SBATCH --account=jonth0
+#SBATCH --partition=standard
+
+module restore eastbay_paper
+cd /home/jonth/eastbay_paper
+source venv/bin/activate
+snakemake --profile simple 
