d0543c4033ddb16b9517e8e9f5627455fe12715a
Jonathan Terhorst <jonth@umich.edu>
2024-02-15 20:36:07 -0500

    more updates

--- scripts/phlash.sh
+++ scripts/phlash.sh
@@ -2,5 +2,6 @@
 
 module load python/3.10.4 
 source /home/jonth/eastbay_paper/venv/bin/activate
-module load cuda/11.8.0 cudnn/11.8-v8.7.0 cupti/11.8.0
+# module load cuda/11.8.0 cudnn/11.8-v8.7.0 cupti/11.8.0
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/jonth/eastbay_paper/venv/lib/python3.10/site-packages/nvidia/cuda_nvrtc/lib"
 TF_CPP_MIN_LOG_LEVEL=0 python3 /home/jonth/eb_dl/paper_repo/scripts/run_phlash.py $@
